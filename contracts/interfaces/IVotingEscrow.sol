441f4987853edbf6eb8b4677a9a0980328589b6e
Sagar133 <sagar@vezures.com>
2021-08-28 14:00:01 +0530

    setting up main branch

--- contracts/interfaces/IVotingEscrow.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-interface IVotingEscrow {
-    function get_last_user_slope(address addr) external returns (int128);
-    function locked__end(address addr) external returns (uint256);
-}
