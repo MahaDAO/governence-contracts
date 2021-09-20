0497382bbe9a141d2f78b5ec2632a9ee93959fe0
Steven Enamakel <enamakel@mahadao.com>
2021-09-20 17:46:27 +0100

    fixed the gas price issues

--- scripts/deployment/deploy_mainnet_matic.py
+++ scripts/deployment/deploy_mainnet_matic.py
@@ -7,11 +7,15 @@ from brownie import (
     AdminUpgradeabilityProxy,
     Contract
 )
+from brownie.network.gas.strategies import GasNowStrategy
+
+gas_strategy = GasNowStrategy("fast")
 
 CONFS = 1
-MAHA_TOKEN_ADDR = ""  # TODO: fill in this address as per the network.
-DEPLOYER = accounts.load('0')  # This will be the admin for the state variables of voting escrow.
-PROXY_ADMIN = ""  # NOTE:- Please change this address. This will be the admin for proxy contract. This cannot call the fallback/delegate function via proxy.
+MAHA_TOKEN_ADDR = "0xedd6ca8a4202d4a36611e2fff109648c4863ae19" # TODO: fill in this address as per the network.
+PROXY_ADMIN = "0x112ae96ce47f9506cdc4668655ae1ad880109d5c" # NOTE:- Please change this address. This will be the 
+# admin for proxy contract. This cannot call the fallback/delegate function via proxy.
+DEPLOYER = accounts.load('0') # This will be the admin for the state variables of voting escrow.
 
 def repeat(f, *args, **kwargs):
     """
@@ -47,7 +51,7 @@ def main():
 
     escrow_without_proxy = repeat(
         VotingEscrow.deploy,
-        {"from": deployer_account, "required_confs": CONFS}
+        {"from": deployer_account, "required_confs": CONFS, "gas_price": gas_strategy} 
     )
     
     proxy = repeat(
@@ -55,7 +59,7 @@ def main():
         escrow_without_proxy,
         PROXY_ADMIN,
         bytes(),
-        {"from": deployer_account, "required_confs": CONFS}
+        {"from": deployer_account, "required_confs": CONFS, "gas_price": gas_strategy}
     )
     escrow_with_proxy = Contract.from_abi('VotingEscrow', proxy, VotingEscrow.abi)
     
@@ -75,16 +79,16 @@ def main():
         "Vote-escrowed MAHA",
         "MAHAX",
         "mahax_0.99",
-        {"from": deployer_account, "required_confs": CONFS}
+        {"from": deployer_account, "required_confs": CONFS, "gas_price": gas_strategy}
     )
     repeat(
         escrow_with_proxy.changeController, 
         PROXY_ADMIN, 
-        {"from": deployer_account, "required_confs": CONFS}
+        {"from": deployer_account, "required_confs": CONFS, "gas_price": gas_strategy}
     )
 
     print('WEEK', escrow_with_proxy.WEEK())
     print('INCREASE_LOCK_AMOUNT', escrow_with_proxy.INCREASE_LOCK_AMOUNT())
     print('SUPPLY', escrow_with_proxy.supply())
 
-    save_output(output_file, 'maticMumbai')
+    save_output(output_file, 'matic')
