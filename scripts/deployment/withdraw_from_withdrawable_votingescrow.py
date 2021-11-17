c885d0e125ec420ee7ece2cdf3019dc9262582ef
Yash Agrawal <yashagrawal18@outlook.com>
2021-11-17 19:30:03 +0530

    Minor fixes

--- scripts/deployment/withdraw_from_withdrawable_votingescrow.py
+++ scripts/deployment/withdraw_from_withdrawable_votingescrow.py
@@ -9,14 +9,8 @@ from brownie import (
 )
 
 
-USE_STRATEGIES = False  # Needed for the ganache-cli tester which doesn't like middlewares
-POA = True
-
 DEPLOYER = accounts.load('0')
 print('Deployer is ', DEPLOYER)
-ARAGON_AGENT = accounts.load('2')
-print('Admin is ', ARAGON_AGENT)
-ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 CONFS = 1
 
 
@@ -33,11 +27,10 @@ def repeat(f, *args, **kwargs):
 
 def main():
     deployer = accounts.at(DEPLOYER)
-    admin = accounts.at(ARAGON_AGENT)
 
     escrow_with_proxy = Contract.from_abi('WithdrawableVotingEscrow', '0x346ed83277744682123CA72bFe54A32C2C75FB9b', WithdrawableVotingEscrow.abi)
 
     repeat(
         escrow_with_proxy.withdraw_locked,
-        {"from": admin, "required_confs": CONFS},
+        {"from": deployer, "required_confs": CONFS},
     )
