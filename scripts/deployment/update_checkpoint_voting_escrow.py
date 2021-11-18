e4ec5f84055f33758e9e7607d0defda02fee452c
Yash Agrawal <yashagrawal18@outlook.com>
2021-11-18 15:14:39 +0530

    updte deployment script

--- scripts/deployment/update_checkpoint_voting_escrow.py
+++ scripts/deployment/update_checkpoint_voting_escrow.py
@@ -36,8 +36,8 @@ def main():
 
     escrow_with_proxy = Contract.from_abi('stakeable_escrow_without_proxy', '0x5996cdFf6b350a9d16723412dAca8c255fc4AcbD', VotingEscrow.abi)
     addrs = ['0x073502E1d77e98bc4f6c526182bb637B46bf53DF'] + [ZERO_ADDRESS] * 99
-    start_times = [1637216040] * 100
-    end_times = [1637223256] * 100
-    amounts = [200 * 1e18] * 100
+    start_times = [1637225004] * 100
+    end_times = [1637232221] * 100
+    amounts = [190 * 1e18] * 100
 
     repeat(escrow_with_proxy.checkpoint, addrs, start_times, end_times, amounts, {"from": deployer, "required_confs": CONFS})
