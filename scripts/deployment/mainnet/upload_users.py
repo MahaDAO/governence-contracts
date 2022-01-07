3131ba7d62e1e587a5189c61183e7c0032b6b619
Yash Agrawal <yashagrawal2829@gmail.com>
2022-01-07 18:51:41 +0530

    feat: update the upload user lock snapshot script

--- scripts/deployment/mainnet/upload_users.py
+++ scripts/deployment/mainnet/upload_users.py
@@ -32,30 +32,34 @@ def main():
     end_times = [int(l[2]) for l in parsed_files]
 
     end_times_final = []
-    week = 7 * 86400
-    today = 1641301063
+    day = 86400
+    week = 7 * day
+    today = 1641480116
 
     for l in end_times:
-        if (l < today + 2 * week): end_times_final += [today + 2 * week]
-        else: end_times_final += [l]
+        if (l < (today + (2 * week + day))): 
+            end_times_final += [today + (2 * week + day)]
+        else: 
+            end_times_final += [l]
 
-    batch_size = 30
-
+    batch_size = 10
     loops = ceil(len(addrs) / batch_size)
     for i in range(loops):
         start_i = i * batch_size
         end_i = (i + 1) * batch_size
-
-        batch_end_times = [str(t) for t in end_times_final[start_i : end_i]]
+        
+        batch_end_times = end_times_final[start_i : end_i]
         batch_amounts = amounts[start_i : end_i]
         batch_addrs = addrs[start_i : end_i]
-
-        print(voting_escrow.staking_contract())
-        # print(batch_addrs, batch_end_times, batch_amounts)
+       
+        if len(batch_end_times) < 100:
+            batch_end_times += [0] * (100 - len(batch_end_times))
+            batch_amounts += [0] * (100 - len(batch_amounts))
+            batch_addrs += [ZERO_ADDRESS] * (100 - len(batch_addrs))
+        
         repeat(
             voting_escrow.manual_add_users,
             batch_addrs, batch_end_times, batch_amounts,
-            {"from": deployer, "required_confs": CONFS, "gas_limit": 20000000, "allow_revert": True},
+            {"from": deployer, "required_confs": CONFS, "allow_revert" : True},
             # gas_limit=20000000, gas_price=0, allow_revert=True
         )
-        exit(-1)
