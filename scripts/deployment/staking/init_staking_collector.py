7d5f580f3de061cb56160f6c2ab8ab2e3890e88b
Steven Enamakel <enamakel@vezures.com>
2022-01-17 14:00:34 +0000

    downgrade access control for pool token

--- scripts/deployment/staking/init_staking_collector.py
+++ scripts/deployment/staking/init_staking_collector.py
@@ -25,7 +25,7 @@ def repeat(f, *args, **kwargs):
 
 def main():
     deployer = accounts.at(DEPLOYER)
-    instance = Contract.from_abi("StakingCollector", '0x39932928416dCbA481Ef73E620fD99165073Bcf7', StakingCollector.abi)
+    instance = Contract.from_abi("StakingCollector", '0x1894dFbEE996502220E2BE203B8e9830622113D9', StakingCollector.abi)
 
     tokens = [
         '0xedd6ca8a4202d4a36611e2fff109648c4863ae19', # maha,
