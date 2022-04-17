7cb10426b153326787c1854885b4d444d75df1d8
Steven Enamakel <enamakel@mahadao.com>
2022-04-17 12:51:34 +0100

    added chainlink oracles

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,21 +1,14 @@
-import { task } from "hardhat/config";
+import * as dotenv from "dotenv";
 
-import { config as dotenvConfig } from "dotenv";
-import { resolve } from "path";
-
-dotenvConfig({ path: resolve(__dirname, "./.env") });
-
-import { HardhatUserConfig } from "hardhat/types";
-import { NetworkUserConfig } from "hardhat/types";
-
-import "@nomiclabs/hardhat-vyper";
+import "@nomiclabs/hardhat-etherscan";
+// import "@nomiclabs/hardhat-vyper";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-waffle";
-
 import "hardhat-gas-reporter";
-import "@nomiclabs/hardhat-etherscan";
+import { HardhatUserConfig, task } from "hardhat/config";
+import { NetworkUserConfig } from "hardhat/types";
+
+dotenv.config();
 
 const chainIds = {
   ganache: 1337,
@@ -29,31 +22,15 @@ const chainIds = {
   "polygon-mumbai": 80001,
 };
 
-const MNEMONIC = process.env.MNEMONIC || "";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(await account.getAddress());
-  }
-});
 
 function createTestnetConfig(
   network: keyof typeof chainIds
 ): NetworkUserConfig {
   const url: string = "https://" + network + ".infura.io/v3/" + INFURA_API_KEY;
   return {
-    accounts: {
-      count: 10,
-      initialIndex: 0,
-      mnemonic: MNEMONIC,
-      path: "m/44'/60'/0'/0",
-    },
+    accounts:
+      process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     chainId: chainIds[network],
     url,
   };
@@ -68,12 +45,6 @@ const config: HardhatUserConfig = {
     sources: "./contracts",
   },
   networks: {
-    hardhat: {
-      accounts: {
-        mnemonic: MNEMONIC,
-      },
-      chainId: chainIds.hardhat,
-    },
     mainnet: createTestnetConfig("mainnet"),
     goerli: createTestnetConfig("goerli"),
     kovan: createTestnetConfig("kovan"),
@@ -87,19 +58,25 @@ const config: HardhatUserConfig = {
       {
         version: "0.8.0",
       },
+      {
+        version: "0.8.1",
+      },
     ],
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      bsc: process.env.BSCSCAN_API_KEY,
+      mainnet: process.env.ETHERSCAN_API_KEY,
+    },
   },
   gasReporter: {
     currency: "USD",
     gasPrice: 100,
-    // enabled: process.env.REPORT_GAS ? true : false,
   },
-  vyper: {
-    version: "0.3.1",
-  },
+  // vyper: {
+  //   version: "0.3.1",
+  // },
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
--- scripts/deploy-chainlink-keeper.ts
+++ scripts/deploy-chainlink-keeper.ts
@@ -4,10 +4,10 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import hre, { ethers } from "hardhat";
-// import { wait } from "./utils";
+import { wait } from "./utils";
 
 async function main() {
-  const collector = "0x6852F8bB8a476fCAD8D6a54aF4a1A61B29146484";
+  const collector = "0x41Ef0505EBaa70eC10F7b8EE8965E269a50cE3eE";
 
   // We get the contract to deploy
   const Factory = await ethers.getContractFactory("ChainlinkCollectorKeeper");
@@ -17,7 +17,7 @@ async function main() {
 
   console.log("ChainlinkCollectorKeeper deployed to:", instance.address);
 
-  //   await wait(60 * 1000); // wait for a minute
+  await wait(60 * 1000); // wait for a minute
 
   await hre.run("verify:verify", {
     address: instance.address,
