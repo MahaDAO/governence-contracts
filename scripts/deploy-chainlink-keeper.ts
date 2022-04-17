35d922b29123375e33d9b0e93de9ec296460dfa9
Steven Enamakel <enamakel@mahadao.com>
2022-04-17 12:14:21 +0100

    mionr changes

--- scripts/deploy-chainlink-keeper.ts
+++ scripts/deploy-chainlink-keeper.ts
@@ -4,27 +4,24 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import hre, { ethers } from "hardhat";
-import { wait } from "./utils";
+// import { wait } from "./utils";
 
 async function main() {
-  const oracles = [
-    "0x93dAe7A7763f95eC86006D9f38A497773ce47380",
-    "0x6852F8bB8a476fCAD8D6a54aF4a1A61B29146484",
-  ];
+  const collector = "0x6852F8bB8a476fCAD8D6a54aF4a1A61B29146484";
 
   // We get the contract to deploy
-  const Factory = await ethers.getContractFactory("ChainlinkTWAPKeeper");
-  const instance = await Factory.deploy(oracles);
+  const Factory = await ethers.getContractFactory("ChainlinkCollectorKeeper");
+  const instance = await Factory.deploy(collector);
 
   await instance.deployed();
 
-  console.log("ChainlinkTWAPKeeper deployed to:", instance.address);
+  console.log("ChainlinkCollectorKeeper deployed to:", instance.address);
 
-  await wait(60 * 1000); // wait for a minute
+  //   await wait(60 * 1000); // wait for a minute
 
   await hre.run("verify:verify", {
     address: instance.address,
-    constructorArguments: [oracles],
+    constructorArguments: [collector],
   });
 }
 
