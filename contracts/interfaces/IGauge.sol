d807a5a9e1e86ff3df016d7c349dc78c109c6ca9
Steven Enamakel <enamakel@mahadao.com>
2022-05-03 03:52:40 +0100

    minor changes

--- contracts/interfaces/IGauge.sol
+++ contracts/interfaces/IGauge.sol
@@ -10,4 +10,36 @@ interface IGauge {
   function claimFees() external returns (uint256 claimed0, uint256 claimed1);
 
   function left(address token) external view returns (uint256);
+
+  /// @notice A checkpoint for marking balance
+  struct Checkpoint {
+    uint256 timestamp;
+    uint256 balanceOf;
+  }
+
+  /// @notice A checkpoint for marking reward rate
+  struct RewardPerTokenCheckpoint {
+    uint256 timestamp;
+    uint256 rewardPerToken;
+  }
+
+  /// @notice A checkpoint for marking supply
+  struct SupplyCheckpoint {
+    uint256 timestamp;
+    uint256 supply;
+  }
+
+  event Deposit(address indexed from, uint256 tokenId, uint256 amount);
+  event Withdraw(address indexed from, uint256 tokenId, uint256 amount);
+  event NotifyReward(
+    address indexed from,
+    address indexed reward,
+    uint256 amount
+  );
+  event ClaimFees(address indexed from, uint256 claimed0, uint256 claimed1);
+  event ClaimRewards(
+    address indexed from,
+    address indexed reward,
+    uint256 amount
+  );
 }
