d91a1b8b5f1191b804c2dffb2e94f61aacaf73e5
0xya <yashagrawal2829@gmail.com>
2022-05-05 13:35:29 +0530

    fix: minor fix

--- scripts/verifyContract.ts
+++ scripts/verifyContract.ts
@@ -8,10 +8,12 @@ export default async function verifyContract(
   address: string,
   constructorArguments: any[]
 ) {
-  await wait(60 * 1000); // wait for a minute
-  console.log(`Verifying contract deployed at address: ${address}`);
-  await hre.run("verify:verify", {
-    address,
-    constructorArguments,
-  });
+  try {
+    await wait(60 * 1000); // wait for a minute
+
+    await hre.run("verify:verify", {
+      address,
+      constructorArguments,
+    });
+  } catch (error: any) {}
 }
