097401b1e5f95b96e6dc4f309ef02211d83e9112
0xya <yashagrawal2829@gmail.com>
2022-05-06 10:30:03 +0530

    feat: update hardhat and testnet deploy script

--- scripts/deployCoreTestnet.ts
+++ scripts/deployCoreTestnet.ts
@@ -9,6 +9,10 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(`Deployer address is ${deployer.address}.`);
 
+  const { provider } = ethers;
+  const estimateGasPrice = await provider.getGasPrice();
+  const gasPrice = estimateGasPrice.mul(3).div(2);
+  console.log(`Gas Price: ${ethers.utils.formatUnits(gasPrice, 'gwei')} gwei`);
   const outputFile: any = {};
 
   // Get all smart contract factories.
@@ -22,36 +26,41 @@ async function main() {
   const bribesFactoryCF = await ethers.getContractFactory(`BaseV1BribeFactory`);
 
   // Deploy all the smart contracts.
-  const gaugeFactoryCI = await gaugeFactoryCF.deploy();
+  const gaugeFactoryCI = await gaugeFactoryCF.deploy({ gasPrice });
   await verifyContract(hre, gaugeFactoryCI.address, []);
 
-  const bribesFactoryCI = await bribesFactoryCF.deploy();
+  const bribesFactoryCI = await bribesFactoryCF.deploy({ gasPrice });
   await verifyContract(hre, bribesFactoryCI.address, []);
 
-  const mahaCI = await mockTokenCF.deploy("MahaDAO", "MAHA");
+  const mahaCI = await mockTokenCF.deploy("MahaDAO", "MAHA", { gasPrice });
   await verifyContract(hre, mahaCI.address, ["MahaDAO", "MAHA"]);
+
+  const daiCI = await mockTokenCF.deploy("DAI Stablecoin", "DAI", { gasPrice });
+  await verifyContract(hre, daiCI.address, ["DAI Stablecoin", "DAI"]);
 
   const emissionControllerCI = await emissionControllerCF.deploy(
     mahaCI.address,
     30 * 24 * 60 * 60,
-    Math.floor(Date.now() / 1000),
-    0
+    Math.floor((Date.now() + 5 * 60 * 1000) / 1000),
+    0,
+    { gasPrice }
   );
   await verifyContract(hre, emissionControllerCI.address, [
     mahaCI.address,
     30 * 24 * 60 * 60,
-    Math.floor(Date.now() / 1000),
+    Math.floor((Date.now() + 5 * 60 * 1000) / 1000),
     0,
   ]);
 
-  const mahaxCI = await mahaxCF.deploy(mahaCI.address);
+  const mahaxCI = await mahaxCF.deploy(mahaCI.address, { gasPrice });
   await verifyContract(hre, mahaxCI.address, [mahaCI.address]);
 
   const voterCI = await voterCF.deploy(
     mahaxCI.address,
     gaugeFactoryCI.address,
     bribesFactoryCI.address,
-    emissionControllerCI.address
+    emissionControllerCI.address,
+    { gasPrice }
   );
   await verifyContract(hre, voterCI.address, [
     mahaxCI.address,
@@ -61,19 +70,30 @@ async function main() {
   ]);
 
   // Link the deployed smart contracts.
-  await emissionControllerCI.setVoter(voterCI.address);
+  await emissionControllerCI.setVoter(voterCI.address, { gasPrice });
 
   // Mint the default faucet.
   await mahaCI.mint(
     emissionControllerCI.address,
-    ethers.BigNumber.from(10).pow(18).mul(1e6)
+    ethers.BigNumber.from(10).pow(18).mul(1e6),
+    { gasPrice }
+  );
+  await daiCI.mint(
+    deployer.address,
+    ethers.BigNumber.from(10).pow(18).mul(1e6),
+    { gasPrice }
   );
   await mahaCI.mint(
     deployer.address,
-    ethers.BigNumber.from(10).pow(18).mul(1e6)
+    ethers.BigNumber.from(10).pow(18).mul(1e6),
+    { gasPrice }
   );
 
   // Create the output.json file.
+  outputFile.DAI = {
+    abi: "MockERC20",
+    address: daiCI.address,
+  };
   outputFile.MahaToken = {
     abi: "MockERC20",
     address: mahaCI.address,
@@ -88,7 +108,7 @@ async function main() {
   };
 
   // Save the output file.
-  fs.writeFileSync(`./output/${network.name}.json`, outputFile);
+  fs.writeFileSync(`./output/${network.name}.json`, JSON.stringify(outputFile));
 
   console.log(`${network.name} deployment complete.`);
 }
