a100248132852ce147fa6bf4598fce10ca788da9
0xya <yashagrawal2829@gmail.com>
2022-05-18 15:32:05 +0530

    fix: remove epoch from emission controller

--- contracts/EmissionController.sol
+++ contracts/EmissionController.sol
@@ -4,20 +4,17 @@ pragma solidity ^0.8.0;
 
 import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-import {Epoch} from "./utils/Epoch.sol";
 import {IVoter} from "./interfaces/IVoter.sol";
 import {IEmissionController} from "./interfaces/IEmissionController.sol";
+import {Context, Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EmissionController is IEmissionController, Epoch {
+contract EmissionController is IEmissionController, Context, Ownable {
   IERC20 public maha;
   IVoter public voter;
 
   constructor(
-    IERC20 _maha,
-    uint256 _period,
-    uint256 _startTime,
-    uint256 _startEpoch
-  ) Epoch(_period, _startTime, _startEpoch) {
+    IERC20 _maha
+  ) {
     maha = _maha;
   }
 
@@ -25,9 +22,12 @@ contract EmissionController is IEmissionController, Epoch {
     voter = _voter;
   }
 
-  function allocateEmission() external override checkEpoch {
+  function allocateEmission() external override {
     uint256 mahaBalance = maha.balanceOf(address(this));
-    maha.approve(address(voter), mahaBalance);
-    voter.notifyRewardAmount(mahaBalance);
+
+    if (mahaBalance > 0) {
+      maha.approve(address(voter), mahaBalance);
+      voter.notifyRewardAmount(mahaBalance);
+    }
   }
 }
