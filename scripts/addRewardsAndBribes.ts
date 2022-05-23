19de95461978696f8278c76583dac1b038d8777b
Yash Agrawal <yashagrawal7098@gmail.com>
2022-05-23 16:20:24 +0530

    feat: update gauge reward adding script

--- scripts/addRewardsAndBribes.ts
+++ scripts/addRewardsAndBribes.ts
@@ -14,7 +14,7 @@ async function main() {
 
   const gauge = await ethers.getContractAt(
     "BaseGaugeV1",
-    "0x8975B4Db0A2Ffb619Ac14Da68cc5AD5f3a33B2B5"
+    "0x281eCb4D21508e1B029d0FC005E7fd9eED2Af86a"
   );
   // const bribe = await ethers.getContractAt(
   //   "BaseV1Bribes",
@@ -22,38 +22,38 @@ async function main() {
   // );
   const amount = BigNumber.from(10).pow(18).mul(1e3);
 
-  // const solid = await ethers.getContractAt(
-  //   "MockERC20",
-  //   "0x62C33009b62B1e972c01820Ac608D9F8992190F5"
-  // );
-  // const dai = await ethers.getContractAt(
-  //   "MockERC20",
-  //   "0x544380B5EE3d1a8485671537a553F61f3c7190f1"
-  // );
+  const solid = await ethers.getContractAt(
+    "MockERC20",
+    "0x62C33009b62B1e972c01820Ac608D9F8992190F5"
+  );
+  const dai = await ethers.getContractAt(
+    "MockERC20",
+    "0x544380B5EE3d1a8485671537a553F61f3c7190f1"
+  );
   const maha = await ethers.getContractAt(
     "MockERC20",
     "0x7aF163582b3ebAAbB7bce03aaDa8C1d76d655a5c"
   );
 
   await maha.mint(deployer.address, amount.mul(3));
-  // await dai.mint(deployer.address, amount.mul(3));
-  // await solid.mint(deployer.address, amount.mul(3));
+  await dai.mint(deployer.address, amount.mul(3));
+  await solid.mint(deployer.address, amount.mul(3));
 
   // await solid.approve(bribe.address, amount.mul(3));
   // await maha.approve(bribe.address, amount.mul(3));
   // await dai.approve(bribe.address, amount.mul(3));
 
-  // await solid.approve(gauge.address, amount.mul(3));
+  await solid.approve(gauge.address, amount.mul(3));
   await maha.approve(gauge.address, amount.mul(3));
-  // await dai.approve(gauge.address, amount.mul(3));
+  await dai.approve(gauge.address, amount.mul(3));
 
   // await bribe.notifyRewardAmount(solid.address, amount);
   // // await bribe.notifyRewardAmount(maha.address, amount);
   // await bribe.notifyRewardAmount(dai.address, amount);
 
-  // await gauge.notifyRewardAmount(solid.address, amount);
+  await gauge.notifyRewardAmount(solid.address, amount);
   await gauge.notifyRewardAmount(maha.address, amount);
-  // await gauge.notifyRewardAmount(dai.address, amount);
+  await gauge.notifyRewardAmount(dai.address, amount);
 }
 
 main().catch((error) => {
