8047adf06b92a46a6de5ef93d0d4bcc75e2fb310
Yash Agrawal <yashagrawal7098@gmail.com>
2022-06-09 12:30:55 +0530

    fix(contracts): add toString conversion in tokenURI

--- contracts/MAHAX.sol
+++ contracts/MAHAX.sol
@@ -1048,7 +1048,7 @@ contract MAHAX is ReentrancyGuard, IVotingEscrow, Ownable {
     require(idToOwner[_tokenId] != address(0), "Query for nonexistent token");
     return
       string(
-        abi.encodePacked("https://images.mahapeople.com/", _tokenId, ".json")
+        abi.encodePacked("https://images.mahapeople.com/", toString(_tokenId), ".json")
       );
   }
 
@@ -1208,6 +1208,28 @@ contract MAHAX is ReentrancyGuard, IVotingEscrow, Ownable {
     }
     // Now dt contains info on how far are we beyond point
     return _supplyAt(point, point.ts + dt);
+  }
+
+  function toString(uint256 value) internal pure returns (string memory) {
+    // Inspired by OraclizeAPI's implementation - MIT license
+    // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
+
+    if (value == 0) {
+      return "0";
+    }
+    uint256 temp = value;
+    uint256 digits;
+    while (temp != 0) {
+      digits++;
+      temp /= 10;
+    }
+    bytes memory buffer = new bytes(digits);
+    while (value != 0) {
+      digits -= 1;
+      buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+      value /= 10;
+    }
+    return string(buffer);
   }
 
   function _burn(uint256 _tokenId) internal {
