d30c59d93c894e53dc835c7c4ec56cf439ef9809
Steven Enamakel <enamakel@mahadao.com>
2022-08-08 16:10:57 +0100

    added mint fn for mock token

--- contracts/test/MockERC20.sol
+++ contracts/test/MockERC20.sol
@@ -2,21 +2,26 @@
 
 pragma solidity ^0.8.0;
 
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockERC20 is ERC20 {
-  uint8 private _decimals;
+    uint8 private _decimals;
 
-  // solhint-disable-next-line
-  constructor(string memory name, string memory symbol, uint8 decimal) ERC20(name, symbol) {
-    _decimals = decimal;
-  }
+    // solhint-disable-next-line
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimal
+    ) ERC20(name, symbol) {
+        _decimals = decimal;
+        _mint(msg.sender, 10**6 * (18**decimal));
+    }
 
-  function decimals() public view virtual override returns (uint8) {
-    return _decimals;
-  }
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
 
-  function mint(address account, uint256 amount) public {
-    _mint(account, amount);
-  }
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
 }
