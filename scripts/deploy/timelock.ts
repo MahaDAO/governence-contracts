e3dfdadd038d1f6b5d393cae2aa92e196b60052b
Steven Enamakel <enamakel@mahadao.com>
2023-01-08 17:36:51 +0530

    deployed 30 day timelock

--- scripts/deploy/timelock.ts
+++ scripts/deploy/timelock.ts
@@ -7,22 +7,21 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log(`Deployer address is ${deployer.address}.`);
 
-  const gnosis = "0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC";
+  const gnosisTeam = "0x6357EDbfE5aDA570005ceB8FAd3139eF5A8863CC";
   const gnosisCommunity = "0x516bd18Ba17f70f08C8C91fE7F9Ee2105DC275d2";
-  const governor = await getOutputAddress("MAHAXGovernor");
   const vetoGovernor = await getOutputAddress("MAHAXVetoGovernor");
 
-  const delay = 86400 * 14;
+  const delay = 86400 * 30;
 
   await deployOrLoadAndVerify(
-    "MAHATimelockController",
+    "MAHATimelockController-30d",
     "MAHATimelockController",
     [
       delay, // uint256 minDelay,
-      gnosis, // address admin,
-      [governor], // address[] memory proposers,
+      gnosisTeam, // address admin,
+      [], // address[] memory proposers,
       [vetoGovernor, gnosisCommunity], // address[] memory cancellors,
-      [gnosis, vetoGovernor, gnosisCommunity], // address[] memory executors
+      [gnosisTeam, vetoGovernor, gnosisCommunity], // address[] memory executors
     ]
   );
 }
