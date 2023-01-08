5f6db3dd828bb9cf2832c33030ee417a920e6a7f
Steven Enamakel <enamakel@mahadao.com>
2023-01-08 19:59:23 +0530

    redeployed emission controller

--- scripts/deploy/emissionController.ts
+++ scripts/deploy/emissionController.ts
@@ -3,45 +3,19 @@ import { ethers, network } from "hardhat";
 import { deployOrLoadAndVerify, getOutputAddress } from "../utils";
 
 async function main() {
-  console.log(`Deploying migrator to ${network.name}`);
+  console.log(`Deploying governance to ${network.name}`);
 
-  const tokenSymbol = "MAHA";
+  const mahaPerEpoch = 2500; // 2500 maha every 2 weeks
 
   const [deployer] = await ethers.getSigners();
   console.log(`Deployer address is ${deployer.address}`);
 
-  const pendingFeeDistributor = await getOutputAddress(
-    `${tokenSymbol}PendingFeeDistributor`
-  );
+  const registry = await getOutputAddress("Registry");
 
-  // Get all the deployed smart contracts.
-  const token = await ethers.getContractAt(
-    "MockERC20",
-    await getOutputAddress(tokenSymbol)
-  );
-
-  const lock = await deployOrLoadAndVerify(
-    `${tokenSymbol}FeeDistributor`,
-    "FeeDistributor",
-    [
-      await getOutputAddress("MAHAXLocker"),
-      token.address,
-      pendingFeeDistributor,
-    ]
-  );
-
-  if (network.name === "goerli") {
-    console.log("minting tokens to the FeeDistributor contract");
-    const tx1 = await token.mint(lock.address, BigNumber.from(10).pow(24));
-    await tx1.wait();
-
-    console.log("minting tokens to the PendingFeeDistributor contract");
-    const tx2 = await token.mint(
-      pendingFeeDistributor,
-      BigNumber.from(10).pow(18).mul(10000)
-    );
-    await tx2.wait();
-  }
+  await deployOrLoadAndVerify("EmissionController", "EmissionController", [
+    registry,
+    BigNumber.from(10).pow(18).mul(mahaPerEpoch),
+  ]);
 }
 
 main().catch((error) => {
