d0e8f4112810180fd8c7c66832184e4564653ee3
Steven Enamakel <enamakel@mahadao.com>
2023-01-11 14:40:28 +0530

    updated the testnet script

--- scripts/testnet/deploy.ts
+++ scripts/testnet/deploy.ts
@@ -1,146 +1,102 @@
-import hre, { ethers, network } from "hardhat";
-import { getOutputAddress, saveABI } from "../utils";
-import verifyContract from "../verifyContract";
+import { ethers, network } from "hardhat";
+import { deployOrLoadAndVerify } from "../utils";
 
 async function main() {
   console.log(`deploying governance to ${network.name}`);
 
-  const now = Math.floor(Date.now() / 1000);
   const [deployer] = await ethers.getSigners();
   console.log(`Deployer address is ${deployer.address}`);
-
-  const merkleProof =
-    "0x8d3c89f0fb1158b832e35caf7eb25c346f0d3e88a0a31b714724c682a2e827ea";
-  const timelockDuration = 5 * 60; // 5 min
 
   const { provider } = ethers;
   const estimateGasPrice = await provider.getGasPrice();
   const gasPrice = estimateGasPrice.mul(3).div(2);
   console.log(`Gas Price: ${ethers.utils.formatUnits(gasPrice, `gwei`)} gwei`);
 
-  // Get all smart contract factories.
-  const mahaxCF = await ethers.getContractFactory(`MAHAXLocker`);
-  const registryCF = await ethers.getContractFactory(`Registry`);
-  const emissionControllerCF = await ethers.getContractFactory(
-    "EmissionController"
-  );
-  const voterCF = await ethers.getContractFactory(`BaseV2Voter`);
-  const gaugeFactoryCF = await ethers.getContractFactory(`BaseV2GaugeFactory`);
-  const bribesFactoryCF = await ethers.getContractFactory(
-    "BaseV2BribesFactory"
-  );
-  const feeDistributorCF = await ethers.getContractFactory("FeeDistributor");
-  const timelockControllerCF = await ethers.getContractFactory(
-    "TimelockController"
-  );
-  const renderingContractCF = await ethers.getContractFactory(
-    "RenderingContract"
-  );
-  const lockMigratorCF = await ethers.getContractFactory("LockMigrator");
-  const mahaxStakerCF = await ethers.getContractFactory("MAHAXStaker");
-  const mahaxGovernorCF = await ethers.getContractFactory("MAHAXGovernor");
-
   // Get all the deployed smart contracts.
-  const mahaCI = await ethers.getContractAt(
-    "MockERC20",
-    getOutputAddress("MAHA")
-  );
-  const arthCI = await ethers.getContractAt(
-    "MockERC20",
-    getOutputAddress("ARTH")
-  );
-  const usdcCI = await ethers.getContractAt(
-    "MockERC20",
-    getOutputAddress("USDC")
-  );
+  const mahaCI = await deployOrLoadAndVerify("MAHA", "MockERC20", [
+    "MAHA",
+    "MAHA",
+    18,
+  ]);
+  const arthCI = await deployOrLoadAndVerify("ARTH", "MockERC20", [
+    "ARTH",
+    "ARTH",
+    18,
+  ]);
 
   // Deploy all the smart contracts.
-  console.log(`deploying timelock controller`);
-  const timelockControllerCI = await timelockControllerCF.deploy(
-    timelockDuration,
-    [deployer.address],
-    [deployer.address],
-    { gasPrice }
+  const timelockControllerCI = await deployOrLoadAndVerify(
+    "MAHATimelockController",
+    "MAHATimelockController",
+    [
+      60 * 60, // uint256 minDelay,
+      deployer.address, // address admin,
+      [deployer.address], // address[] memory proposers,
+      [deployer.address], // address[] memory cancellors,
+      [deployer.address], // address[] memory executors
+    ]
   );
-  await timelockControllerCI.deployed();
 
-  console.log(`deploying registry`);
-  const registryCI = await registryCF.deploy({ gasPrice });
-  console.log(`tx hash: `, registryCI.deployTransaction.hash);
-  await registryCI.deployed();
-
-  console.log(`deploying rendering contract`);
-  const renderingContractCI = await renderingContractCF.deploy({ gasPrice });
-  console.log(`tx hash: `, renderingContractCI.deployTransaction.hash);
-  await renderingContractCI.deployed();
+  const registryCI = await deployOrLoadAndVerify("Registry", "Registry", []);
+  const renderingContractCI = await deployOrLoadAndVerify(
+    "RenderingContract",
+    "RenderingContract",
+    []
+  );
 
   console.log(`deploying governor`);
-  const governorCI = await mahaxGovernorCF.deploy(
-    registryCI.address,
-    timelockControllerCI.address,
-    100,
-    1000,
-    ethers.BigNumber.from(10).pow(18).mul(250),
-    ethers.BigNumber.from(10).pow(18).mul(1000), // 1000 maha quorum
-    { gasPrice }
+  const governorCI = await deployOrLoadAndVerify(
+    "MAHAXGovernor",
+    "MAHAXGovernor",
+    [
+      registryCI.address,
+      timelockControllerCI.address,
+      100,
+      1000,
+      ethers.BigNumber.from(10).pow(18).mul(250),
+      ethers.BigNumber.from(10).pow(18).mul(1000), // 1000 maha quorum
+      100,
+    ]
   );
-  console.log(`tx hash: `, governorCI.deployTransaction.hash);
-  await governorCI.deployed();
 
   console.log(`deploying staker`);
-  const mahaxStakerCI = await mahaxStakerCF.deploy(
-    registryCI.address,
-    timelockControllerCI.address,
-    {
-      gasPrice,
-    }
+  const mahaxStakerCI = await deployOrLoadAndVerify(
+    "MAHAXStaker",
+    "MAHAXStaker",
+    [registryCI.address, timelockControllerCI.address]
   );
-  console.log(`tx hash: `, mahaxStakerCI.deployTransaction.hash);
-  await mahaxStakerCI.deployed();
 
-  console.log(`deploying locker`);
-  const mahaxCI = await mahaxCF.deploy(
+  const mahaxCI = await deployOrLoadAndVerify("MAHAXLocker", "MAHAXLocker", [
     registryCI.address,
     deployer.address,
     renderingContractCI.address,
     10000,
-    { gasPrice }
+  ]);
+
+  const emissionControllerCI = await deployOrLoadAndVerify(
+    "EmissionController",
+    "EmissionController",
+    [registryCI.address, "1000000000000000000"]
   );
-  await mahaxCI.deployed();
-  console.log(`tx hash: `, mahaxCI.deployTransaction.hash);
 
-  console.log(`deploying merkle proof migrator`);
-  const lockMigratorCI = await lockMigratorCF.deploy(
-    merkleProof,
-    mahaCI.address,
-    mahaxCI.address,
-    { gasPrice }
+  const voterCI = await deployOrLoadAndVerify("BaseV2Voter", "BaseV2Voter", []);
+
+  // Deploy fee distributor contracts.
+  console.log(`deploying MAHA fee distributor.`);
+  const mahaFeeDistributorCI = await deployOrLoadAndVerify(
+    "MAHAFeeDistributor",
+    "FeeDistributor",
+    [mahaxCI.address, mahaCI.address, deployer.address]
   );
-  await lockMigratorCI.deployed();
-  console.log(`tx hash: `, lockMigratorCI.deployTransaction.hash);
 
-  console.log(`deploying gauge and bribe factories`);
-  const gaugeFactoryCI = await gaugeFactoryCF.deploy({ gasPrice });
-  await gaugeFactoryCI.deployed();
-  console.log(`tx hash: `, gaugeFactoryCI.deployTransaction.hash);
-
-  const bribesFactoryCI = await bribesFactoryCF.deploy({ gasPrice });
-  await bribesFactoryCI.deployed();
-  console.log(`tx hash: `, bribesFactoryCI.deployTransaction.hash);
-
-  console.log(`deploying emission controller`);
-  const emissionControllerCI = await emissionControllerCF.deploy(
-    registryCI.address,
-    "1000000000000000000",
-    { gasPrice }
+  const arthFeeDistributorCI = await deployOrLoadAndVerify(
+    "ARTHFeeDistributor",
+    "FeeDistributor",
+    [mahaxCI.address, arthCI.address, deployer.address]
   );
-  await emissionControllerCI.deployed();
-  console.log(`tx hash: `, emissionControllerCI.deployTransaction.hash);
 
-  console.log(`deploying gauge voter`);
-  const voterCI = await voterCF.deploy(registryCI.address, { gasPrice });
-  await voterCI.deployed();
-  console.log(`tx hash: `, voterCI.deployTransaction.hash);
+  console.log("init voter");
+  await voterCI.initialize(registryCI.address, deployer.address);
 
   console.log(`setting contracts in registry`);
   await registryCI.initialize(
@@ -152,81 +108,6 @@ async function main() {
     emissionControllerCI.address,
     deployer.address,
     { gasPrice }
-  );
-
-  // Deploy fee distributor contracts.
-  console.log(`deploying MAHA fee distributor.`);
-  const mahaFeeDistributorCI = await feeDistributorCF.deploy(
-    mahaxCI.address,
-    now,
-    mahaCI.address,
-    deployer.address,
-    deployer.address
-  );
-  await mahaFeeDistributorCI.deployed();
-  console.log(`tx hash: `, mahaFeeDistributorCI.deployTransaction.hash);
-
-  console.log(`deploying ARTH fee distributor.`);
-  const arthFeeDistributorCI = await feeDistributorCF.deploy(
-    mahaxCI.address,
-    now,
-    arthCI.address,
-    deployer.address,
-    deployer.address
-  );
-  await arthFeeDistributorCI.deployed();
-  console.log(`tx hash: `, arthFeeDistributorCI.deployTransaction.hash);
-
-  console.log(`deploying WETH fee distributor.`);
-  const usdcFeeDistributorCI = await feeDistributorCF.deploy(
-    mahaxCI.address,
-    now,
-    usdcCI.address,
-    deployer.address,
-    deployer.address
-  );
-  await usdcFeeDistributorCI.deployed();
-  console.log(`tx hash: `, usdcFeeDistributorCI.deployTransaction.hash);
-
-  // Create the output.json file.
-
-  await saveABI("MAHAXStaker", "MAHAXStaker", mahaxStakerCI.address);
-  await saveABI(
-    "TimelockController",
-    "TimelockController",
-    timelockControllerCI.address
-  );
-  await saveABI("MAHAXGovernor", "MAHAXGovernor", governorCI.address);
-  await saveABI("MAHALocker", "MAHAXLocker", mahaxCI.address);
-  await saveABI("GaugeVoter", "BaseV2Voter", voterCI.address);
-  await saveABI("Registry", "IRegistry", registryCI.address);
-  await saveABI("GaugeFactory", "IGaugeFactory", gaugeFactoryCI.address);
-  await saveABI("BribesFactory", "IBribeFactory", bribesFactoryCI.address);
-  await saveABI(
-    "EmissionController",
-    "IEmissionController",
-    emissionControllerCI.address
-  );
-  await saveABI("LockMigrator", "ILockMigrator", lockMigratorCI.address);
-  await saveABI(
-    "RenderingContract",
-    "RenderingContract",
-    renderingContractCI.address
-  );
-  await saveABI(
-    "MAHAFeeDistributor",
-    "FeeDistributor",
-    mahaFeeDistributorCI.address
-  );
-  await saveABI(
-    "ARTHFeeDistributor",
-    "FeeDistributor",
-    arthFeeDistributorCI.address
-  );
-  await saveABI(
-    "USDCFeeDistributor",
-    "FeeDistributor",
-    usdcFeeDistributorCI.address
   );
 
   // Mint to the emissions controller.
@@ -250,70 +131,6 @@ async function main() {
     ethers.BigNumber.from(10).pow(18).mul(1e6),
     { gasPrice }
   );
-  console.log(`Minting USDC to Fee distributor`);
-  await usdcCI.mint(
-    usdcFeeDistributorCI.address,
-    ethers.BigNumber.from(10).pow(6).mul(1e6),
-    { gasPrice }
-  );
-
-  console.log(`Verifying contracts:\n`);
-  await verifyContract(hre, timelockControllerCI.address, [
-    timelockDuration,
-    [deployer.address],
-    [deployer.address],
-  ]);
-
-  await verifyContract(hre, registryCI.address, []);
-
-  await verifyContract(hre, governorCI.address, [
-    registryCI.address,
-    timelockControllerCI.address,
-    100,
-    1000,
-    ethers.BigNumber.from(10).pow(18).mul(250),
-    ethers.BigNumber.from(10).pow(18).mul(1000),
-  ]);
-
-  await verifyContract(hre, mahaxStakerCI.address, [
-    registryCI.address,
-    timelockControllerCI.address,
-  ]);
-
-  await verifyContract(hre, mahaxCI.address, [
-    registryCI.address,
-    deployer.address,
-    renderingContractCI.address,
-    10000,
-  ]);
-
-  await verifyContract(hre, gaugeFactoryCI.address, []);
-  await verifyContract(hre, bribesFactoryCI.address, []);
-  await verifyContract(hre, emissionControllerCI.address, [
-    registryCI.address,
-    "1000000000000000000",
-  ]);
-
-  await verifyContract(hre, lockMigratorCI.address, [
-    merkleProof,
-    mahaCI.address,
-    mahaxCI.address,
-  ]);
-
-  await verifyContract(hre, renderingContractCI.address, []);
-
-  await verifyContract(hre, voterCI.address, [
-    registryCI.address,
-    deployer.address,
-  ]);
-
-  await verifyContract(hre, mahaFeeDistributorCI.address, [
-    mahaxCI.address,
-    now,
-    mahaCI.address,
-    deployer.address,
-    deployer.address,
-  ]);
 
   console.log(`Governance deployment on ${network.name} complete.`);
 }
